46d5d348b9e8d4b0bd71689a93deea011c4e8185
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2025-03-15 11:26:35 +0530

    t

---
+++ Asynchronous/two.js
@@ -0,0 +1,6 @@
+setTimeout(()=>{console.log("Request 1")},7000)
+//JS inbuilt function executing provided function
+//after certain time interval
+setTimeout(()=>{console.log("Request 2")},3000)
+setTimeout(()=>{console.log("Request 3")},1000)
+setTimeout(()=>{console.log("Request 4")},2000)
